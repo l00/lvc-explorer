6f557292fb933c0134c6c7b0e18c4e8d679fc34e
l00 <l00@1i.rip>
2026-04-02 03:01:23 +0300

    Added rate limiting

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
 import rpc
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
+limiter = Limiter(key_func=get_remote_address)
 
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 class Block(BaseModel):
     height: int
@@ -24,7 +31,8 @@ class HeightResponse(BaseModel):
     height: int
 
 @app.get("/last_block_header")
-async def get_last_block_header() -> LastBlockHeaderResponse:
+@limiter.limit("30/minute;2/second")
+async def get_last_block_header(request: Request) -> LastBlockHeaderResponse:
     raw = rpc.get_last_block_header()
     block = raw["result"]["block_header"]
     return LastBlockHeaderResponse(
@@ -34,12 +42,14 @@ async def get_last_block_header() -> LastBlockHeaderResponse:
     )
 
 @app.get("/height")
-async def get_height() -> HeightResponse:
+@limiter.limit("30/minute;2/second")
+async def get_height(request: Request) -> HeightResponse:
     raw = rpc.get_last_block_header()
     return HeightResponse(height=raw["result"]["block_header"]["height"])
 
-@app.get("/blocks") 
-async def get_blocks_list(height: int) -> BlocksResponse:
+@app.get("/blocks")
+@limiter.limit("30/minute;2/second")
+async def get_blocks_list(height: int, request: Request) -> BlocksResponse:
     raw = rpc.get_blocks_list(height)
     return BlocksResponse(
         status=raw["result"]["status"],
@@ -47,11 +57,13 @@ async def get_blocks_list(height: int) -> BlocksResponse:
     )
 
 @app.get("/tx_details")
-async def get_tx_details(hash: str):
+@limiter.limit("30/minute;2/second")
+async def get_tx_details(hash: str, request: Request):
     response = rpc.get_tx_details(hash)
     return response
 
 @app.get("/block_details")
-async def get_block_details(hash: str):
+@limiter.limit("30/minute;2/second")
+async def get_block_details(hash: str, request: Request):
     response =  rpc.get_block_details(hash)
     return response
