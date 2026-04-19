d7d0d8fbb6f607f5280f96f79c5468084c366a81
l00 <257333660+l00@users.noreply.github.com>
2026-04-19 14:32:33 +0300

    Fixed CORS

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ import rpc
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
+from fastapi.middleware.cors import CORSMiddleware
 
 limiter = Limiter(key_func=get_remote_address)
 
 app = FastAPI()
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Block(BaseModel):
     height: int
